fc117565c06f9ac5ecf8d4d47e3e4a8fd166e6a1
Vandenynas <stlo.subs@gmail.com>
2022-05-25 17:02:15 +0300

    Changed connect with me text. Added class to the social media icons

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,21 +5,15 @@ class SocialMedia extends Component {
         return (
             <footer className="container">
                 <div className="row">
-
                     <div className="col-md-12">
-                        <div className="card">
-                            <div className="card-body">
-                                <h4 className="card-title">You can connect with me here</h4>
-                                <div>
-                                    <a href="https://www.linkedin.com/in/david-de-esteban" target="_blank" rel="noreferrer noopener">
-                                        <img src="LinkedInLogo.svg" alt="LinkedIn Logo"></img>
-                                    </a>
-                                    <a href="https://github.com/Vandenynas" target="_blank" rel="noreferrer noopener">
-                                    <img src="GitHubLogo.svg" alt="GitHub Logo"></img>
-                                    </a>
-                                    
-                                </div>
-                            </div>
+                        <h4 className="card-title">Connect with me</h4>
+                        <div>
+                            <a href="https://www.linkedin.com/in/david-de-esteban" target="_blank" rel="noreferrer noopener">
+                                <img src="LinkedInLogo.svg" alt="LinkedIn Logo" className="filter-blue"></img>
+                            </a>
+                            <a href="https://github.com/Vandenynas" target="_blank" rel="noreferrer noopener">
+                                <img src="GitHubLogo.svg" alt="GitHub Logo" className="filter-blue"></img>
+                            </a>
                         </div>
                     </div>
                 </div>
