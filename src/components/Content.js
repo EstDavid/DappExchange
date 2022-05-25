73bb2362aa8a3330fdbad20b2941d38291150482
Vandenynas <stlo.subs@gmail.com>
2022-05-25 17:22:03 +0300

    Simplified and improved behavior of alerts

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import { connect } from 'react-redux';
 import { 
     accountSelector,
@@ -13,25 +13,46 @@ import {
     loadExchange,
     loadAllOrders,
     subscribeToEvents,
-    accountChanged } from '../store/interactions';
+    accountChanged, 
+    unloadToken,
+    unloadExchange,
+    unloadAccount} from '../store/interactions';
 import Trades from './Trades';
 import OrderBook from './OrderBook';
 import MyTransactions from './MyTransactions';
 import PriceChart from './PriceChart';
 import Balance from './Balance';
 import NewOrder from './NewOrder';
+import {OnboardingButton} from './Onboarding'
 
 import {dappNetwork, dappNetworkHex} from '../helpers'
 
 const ethereum = window.ethereum;
 
 const metamaskAlert = () => {
-    if(typeof ethereum === undefined) {
-        return (
+    return (
+        <div>
             <div className="alert alert-primary" role="alert">
-                Please install Metamask
+                Please install MetaMask in order to access the Dapp
+                <OnboardingButton />
             </div>
-    )        
+        </div>
+    )
+}
+
+const metamaskLoginAlert = () => {
+    if (!ethereum.isConnected()) {
+        return (
+            <div>
+                <div className="alert alert-primary" role="alert">
+                    Please login with MetaMask
+                    <button className="btn btn-primary alert-button" onClick={(event) => {
+                        event.preventDefault();
+                        ethereum.request({ method: 'eth_requestAccounts' })
+                    }}>Login with Metamask</button>
+                </div>
+            </div>
+        )
     }
 }
 
@@ -49,9 +70,15 @@ const changeNetwork = async () => {
               method: 'wallet_addEthereumChain',
               params: [
                 {
-                  chainId: '0xf00',
-                  chainName: '...',
-                  rpcUrls: ['https://...'] /* ... */,
+                  chainId: dappNetworkHex,
+                  chainName: 'Rinkeby',
+                  rpcUrls: ['https://rinkeby.infura.io/v3/'] /* ... */,
+                  nativeCurrency: {
+                      name: 'Ethereum',
+                      symbol: 'ETH',
+                      decimals: 18
+                  },
+                  blockExplorerUrls: 'https://rinkeby.etherscan.io'
                 },
               ],
             });
@@ -63,26 +90,8 @@ const changeNetwork = async () => {
       }      
 }
 
-const accountAlert = (props) => {
-    if (ethereum.networkVersion === null) {
-        return (
-            <div>
-                <div className="alert alert-primary" role="alert">
-                    Please login with MetaMask
-                    <button className="btn btn-primary alert-button" onClick={(event) => {
-                        event.preventDefault();
-                        ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
-                            loadAccount(props.web3, props.dispatch)
-                        });
-                    }}>Enable Ethereum</button>
-                </div>
-            </div>
-        )
-    }
-}
-
 const changeNetworkAlert = (props) => {
-    if (ethereum.networkVersion !== dappNetwork && props.account !== undefined)
+    if (ethereum.networkVersion !== dappNetwork)
         return (
             <div>
                 <div className="alert alert-primary" role="alert">
@@ -96,33 +105,21 @@ const changeNetworkAlert = (props) => {
         );
 }
 
-const tokenAlert = (tokenLoaded) => {
-    if (!tokenLoaded) {
-        return (
-            <div className="alert alert-primary" role="alert">
-                Token contract not deployed to the current network
-            </div>
-        )
-    }
-}
-
-const exchangeAlert = (exchangeLoaded) => {
-    if(!exchangeLoaded) {
-        return (
-            <div className="alert alert-primary" role="alert">
-                Exchange contract not deployed to the current network
-            </div>
-        )
-    }
-}
-
 const showAlerts = (props) => {
     return (
         <div>
-            <div style={{ height: "8px" }}></div>
-            {metamaskAlert(props.web3Loaded)}
-            {accountAlert(props)}
+            {metamaskLoginAlert()}
             {changeNetworkAlert(props)}
+        </div>
+
+    )
+}
+
+const alerts = (props) => {
+    return (
+        <div>
+            <div style={{ height: "8px" }}></div>
+            {ethereum ? showAlerts(props) : metamaskAlert()}
         </div>
     )
 }
@@ -150,47 +147,63 @@ const showContent = () => {
 
 class Content extends Component {
     componentDidMount() {
-        this.loadBlockchainData(this.props.dispatch);
-      }
-      async loadBlockchainData(dispatch) {
-        const networkVersion = await ethereum.networkVersion;
+        if (typeof ethereum !== 'undefined') {
+            this.loadBlockchainData(this.props.dispatch);
+        }
+    }
+
+    async loadBlockchainData(dispatch) {
         const web3 = await loadWeb3(dispatch);
+        window.ethereum.on('chainChanged', async (chainId) => {
+            if (chainId === dappNetworkHex) {
+                this.loadBlockchainData(this.props.dispatch);
+            } else {
+                await unloadToken(dispatch);
+                await unloadExchange(dispatch);
+                // await unloadAccount(dispatch);
+                await loadWeb3(dispatch);
+            }
+        });
         if (web3 !== undefined) {
             const networkId = await web3.eth.net.getId();
             await loadAccount(web3, dispatch);
 
             const token = await loadToken(web3, networkId, dispatch);
-            if(!token) {
+            if (!token) {
                 window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.');
                 return;
             }
             const exchange = await loadExchange(web3, networkId, dispatch);
-            if(!exchange) {
-            // window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.');
-            return;
+            if (!exchange) {
+                // window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.');
+                return;
             }
-            if(exchange !== undefined) {
+            if (exchange !== undefined) {
                 await loadAllOrders(exchange, dispatch);
                 await subscribeToEvents(exchange, dispatch);
             } else {
                 // window.alert('Exchange is not defined. Please select another network with Metamask.');
             }
-            window.ethereum.on('accountsChanged', () => {
-                accountChanged(this.props.dispatch, web3, exchange, token);
+            window.ethereum.on('accountsChanged', async () => {
+                await loadAccount(web3, dispatch);
+                if (this.props.account === undefined) {
+                    await accountChanged(this.props.dispatch, web3, exchange, token);
+                } else {
+                    await accountChanged(this.props.dispatch, web3, exchange, token);
+                }
             });
-    
         }
-      }
+    }
     render() {
-        const contentAvailable = 
-                                    this.props.contractsLoaded && 
-                                    this.props.account !== undefined;
+        const contentAvailable =
+            this.props.contractsLoaded &&
+            this.props.account !== undefined;
         return (
             <div>
-                {contentAvailable ? showContent() : showAlerts(this.props)}
+                {contentAvailable ? showContent() : alerts(this.props)}
             </div>
         );
-    }    
+    }
 }
 
 function mapStateToProps(state) {
