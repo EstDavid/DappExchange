937b4218b0a36b17aae4064f426338bc32362977
Vandenynas <stlo.subs@gmail.com>
2022-05-27 22:29:59 +0300

    Modified jquery functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap';
 import App from './components/App';
 import configureStore from './store/configureStore';
 import * as serviceWorker from './serviceWorker'
+
+$('.dropdown-toggle').dropdown("toggle");
+
+$('.collapse').collapse() 
+
 
 ReactDOM.render(
   <Provider store={configureStore()}>
